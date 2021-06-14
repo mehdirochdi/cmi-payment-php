da3ff74e504a4eeeaeb87727c1a18e7904aff1ff
mehdi rochdi <mehdi.rochdi@gmail.com>
2021-06-14 21:55:13 +0000

    add composer.lock on gitignore

--- example/process.php
+++ example/process.php
@@ -3,13 +3,14 @@
 
 require '../vendor/autoload.php'; // AUTOLOAD FROM COMPOSER
 
+$base_url="http://cmi-php.local/example";
 $client = new CMI\CmiClient([
-    'storekey' => 'TEST1234', // STOREKEY
-    'clientid' => '600002599', // CLIENTID
-    'oid' => '136ABC', // COMMAND ID IT MUST BE UNIQUE
-    'shopurl' => 'YOUR_DOMAIN_HERE', // SHOP URL FOR REDIRECTION
-    'okUrl' => 'YOUR_DOMAIN_HERE/okFail.php', // REDIRECTION AFTER SUCCEFFUL PAYMENT
-    'failUrl' => 'YOUR_DOMAIN_HERE/okFail.php', // REDIRECTION AFTER FAILED PAYMENT
+    'storekey' => '', // STOREKEY
+    'clientid' => '', // CLIENTID
+    'oid' => '137ABC', // COMMAND ID IT MUST BE UNIQUE
+    'shopurl' => $base_url, // SHOP URL FOR REDIRECTION
+    'okUrl' => $base_url.'/okFail.php', // REDIRECTION AFTER SUCCEFFUL PAYMENT
+    'failUrl' => $base_url.'/okFail.php', // REDIRECTION AFTER FAILED PAYMENT
     'email' => 'mehdi.rochdi@gmail.com', // YOUR EMAIL APPEAR IN CMI PLATEFORM
     'BillToName' => 'mehdi rochdi', // YOUR NAME APPEAR IN CMI PLATEFORM
     'BillToCompany' => 'company name', // YOUR COMPANY NAME APPEAR IN CMI PLATEFORM
@@ -20,7 +21,7 @@ $client = new CMI\CmiClient([
     'BillToCountry' => '504', // YOUR COUNTRY APPEAR IN CMI PLATEFORM NOT REQUIRED (504=MA)
     'tel' => '0021201020304', // YOUR PHONE APPEAR IN CMI PLATEFORM NOT REQUIRED
     'amount' => $_POST['amount'], // RETRIEVE AMOUNT WITH METHOD POST
-    'CallbackURL' => 'YOUR_DOMAIN_HERE/callback.php', // CALLBACK
+    'CallbackURL' => $base_url.'/callback.php', // CALLBACK
 ]);
 
 // OPTIONAL ATTRIBUTES
